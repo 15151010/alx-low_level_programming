dfb6baaa5fea3c96a83217974e8d1385028dc6d1
gaberr <me@example.com>
2023-10-10 16:41:12 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,12 @@
-#include"main.h"
+#include "main.h"
+
 /**
  * print_alphabet_x10 - function that will print the alphabet 10 time
  *
 */
 void print_alphabet_x10(void)
 {
-	int line, ch;
+	int line. ch;
 	for (line = 0; line <= 9; line++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
