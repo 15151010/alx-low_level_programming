e48a417c8e323db082a162c1d9aeefcf3629fcaf
gaberr <me@example.com>
2023-10-10 18:33:02 -0700

    a

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - function that computes the absolute
+ * value of an integer
+ *
+ * @n: tekes in integet type input for function
+ *
+ * Return: Alweays 0 (Success)
+*/
+
+int _ads(int n)
+{
+	if (n < 0)
+		n= (-1) * n;
+	return (n);
+}
