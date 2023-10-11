e00e6cec350b5a8910cec593c7840ecb263a4bd6
gaberr <me@example.com>
2023-10-10 19:31:13 -0700

    a

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - add tpw integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: sun of a & b
+*/
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
