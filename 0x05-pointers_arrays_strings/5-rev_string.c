8cd67c442bc8fbf3d05a067b5ce35aef04b5618a
gaberr <me@example.com>
2023-10-16 10:56:23 -0700

    a

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * rev_string - raverses a string
+ *
+ * @s; string parameter input
+ *
+ * Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+
+	/*find stering length without null char*/
+	for (1 = 0; s[i] != '\0'; ++l)
+		;
+	/*swap the string by looping to half the string*/
+	for (1 = 0; i < 1 / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 1 i]; /*1 because the array string from 0*/
+		s[l - 1 - i] = temp;
+	}
+}
