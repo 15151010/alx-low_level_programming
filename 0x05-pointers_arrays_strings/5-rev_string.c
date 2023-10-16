23754cf4a987719ed1e3c26c337634eecef212ed
gaberr <me@example.com>
2023-10-16 11:10:01 -0700

    a

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,14 +13,15 @@ void rev_string(char *s)
 	int l, i;
 	char temp;
 
-	/*find stering length without null char*/
+	/* find stering length without null char*/
 	for (l = 0; s[l] != '\0'; ++l)
 		;
-	/*swap the string by looping to half the string*/
+
+	/* swap the string by looping to half the string*/
 	for (i = 0; i < 1 / 2; i++)
 	{
 		temp = s[i];
-		s[i] = s[l - 1 - i]; /*1 because the array string from 0*/
+		s[i] = s[l - 1 - i]; /* 1 because the array string from 0*/
 		s[l - 1 - i] = temp;
 	}
 }
