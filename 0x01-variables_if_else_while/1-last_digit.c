70aa86d0b834948df4aa5a88c2da3c8338ed53d9
gaberr <me@example.com>
2023-10-15 19:47:51 -0700

    a

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
+#include <sdtio.h>
 
 /**
  * main - enty point
  *
  * Description: print the value of n status
- *greater than, is zero and is not less than 6.
+ *	greater than, is zero and is not less than 6.
  *
  * Return: 0 (success)
 */
